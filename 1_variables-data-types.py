076aae7b369405afa52901821ab85656e05c21c6
Kazi Hashibur Rahman <99647279+ogh-bnz@users.noreply.github.com>
2024-09-25 20:24:36 +0600

    Variables & Data Types

---
+++ 1_variables-data-types.py
@@ -0,0 +1,11 @@
+# Let's Start
+Hello = "Hello World"
+theNum = 887
+
+print(Hello) print(theNum)
+
+text, numb, floatNum = "This is my text", 599, 6.5
+
+print(text)
+print(numb)
+print(type(text))
