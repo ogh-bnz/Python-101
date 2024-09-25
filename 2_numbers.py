c823520a33d2bd9832bc113246a4bd7d279386db
Kazi Hashibur Rahman <99647279+ogh-bnz@users.noreply.github.com>
2024-09-25 20:25:24 +0600

    Numbers

---
+++ 2_numbers.py
@@ -0,0 +1,54 @@
+# Normal Numbers
+
+t1_int = 1
+t1_float = 1.2
+
+print(type(t1_int))
+print(type(t1_float))
+
+# Complex Numbers
+
+t1_complex = 3.14j
+
+print(t1_complex)
+print(type(t1_int))
+
+# Hex Numbers
+
+t1_hex = 0xa
+
+print(t1_hex)
+print(type(t1_hex))
+
+# Octal Numbers
+
+t1_octal = 0o10
+
+print(t1_octal)
+print(type(t1_octal))
+
+# Here are the example of a various number sum
+
+print(1 + 0x1 + 0o1) # one + one + one = three
+print(10 + 0xa + 0o12) # ten + ten + ten = thirty
+
+
+#! More Values
+
+# Abselute Value
+
+print(abs(4))
+print(abs(-4))
+
+# Round Values
+
+print(round(8.6))
+print(round(8.4))
+
+#! We can also use "bin" to print out the binary
+
+print(bin(9))
+
+#! We can also use "hex" to print out the hexadecimal Number
+
+print(hex(7))
