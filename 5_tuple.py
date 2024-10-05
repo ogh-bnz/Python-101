567185ec080e409a958fe8299376ec86069db40b
Kazi Hashibur Rahman <99647279+ogh-bnz@users.noreply.github.com>
2024-10-05 13:52:22 +0600

    Python Tuple

---
+++ 5_tuple.py
@@ -0,0 +1,15 @@
+tuple_items = ("item1", "item2", "item3")
+print(tuple_items)
+print(type(tuple_items))
+
+tuple_number = ("1", "2", "3", "4")
+print(tuple_number)
+print(type(tuple_number))
+
+tuple_repeat = ("Combain!" ,) * 3
+print(tuple_repeat)
+print(type(tuple_repeat))
+
+tuple_combained = tuple_items + tuple_number
+print(tuple_combained)
+print(type(tuple_combained))
